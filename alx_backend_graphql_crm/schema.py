5835ea22a280a7dcf39ab331c9bd437173a06f78
Thamsanqa <thamzafaniso@gmail.com>
2025-06-28 18:47:48 +0200

    Initial GraphQL setup with hello query

---
+++ alx_backend_graphql_crm/schema.py
@@ -0,0 +1,11 @@
+# schema.py
+
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
+
+schema = graphene.Schema(query=Query)
